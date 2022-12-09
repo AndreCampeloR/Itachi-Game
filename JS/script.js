e2a3b79a55de6c1c234fcc87790dd2a1e5ca73be
AndreCampeloR <decocampeloreis@gmail.com>
2022-12-09 11:19:47 -0300

    att game

--- JS/script.js
+++ JS/script.js
@@ -1,99 +1,118 @@
 const gameover = document.querySelector('.game-board-gameover');
-const itachi = document.querySelector('.game-board-itachi');
+const startgame = document.querySelector('.game-board-startgame');
+const itachi = document.querySelector('.game-board-itachi');                      //elementos 
 const parede = document.querySelector('.game-board-parede');
 const nuvens = document.querySelector('.game-board-nuvens');
 const olhos = document.querySelector('.game-board-olhos');
 const maiorPontuacao = document.querySelector('.record-timer')
-  
-  
-  
-    
-    let timer = 0;
-    let display = document.querySelector('.pontuacao-timer'); // selecionando o timer
-    let id = null;
-    window.onload = function() {
-        if(!id) {
-            id = setInterval(function () {
-                display.textContent = timer++;
-                if(timer >= 80 && timer <90){
-                    olhos.style.display = 'block'
-                }
-                if (timer >= 91 && timer <391) {
-                    parede.style.animation = 'parede-animation 1.5s infinite linear';
-                    olhos.style.display = 'none' ;
-                }  
-                if(timer >= 380 && timer <= 390){
-                    olhos.style.display = 'block'
-                }
+
+
+
+let timer = 0;
+let display = document.querySelector('.pontuacao-timer'); // selecionando o timer
+let id = null;
+let pontuacao
+
+
+
+startgame.addEventListener('click', function iniciar() {
+    startgame.style.display = 'none';
+    if(!id) {
+        id = setInterval(function () {
+            display.textContent = timer++;
+            if(timer >= 80 && timer <90){
+                olhos.style.display = 'block'
+            }
+            if (timer >= 91 && timer <391) {
+                parede.style.animation = 'parede-animation 1.5s infinite linear';
+                olhos.style.display = 'none' ;
+            }  
+            if(timer >= 380 && timer <= 390){
+                olhos.style.display = 'block'
+            }
+            
+            if(timer >= 391){
+                parede.style.animation = 'parede-animation 1.0s infinite linear';
+                olhos.style.display = 'none' ;
+            }
+            if(timer >= 480 && timer <= 490){
+                olhos.style.display = 'block'
+            }
                 
-                if(timer >= 391){
-                    parede.style.animation = 'parede-animation 1.0s infinite linear';
-                    olhos.style.display = 'none' ;
-                }
-                if(timer >= 480 && timer <= 490){
-                    olhos.style.display = 'block'
-                }
-                
-                if(timer >= 491){
-                    parede.style.animation = 'parede-animation 0.8s infinite linear';
-                    olhos.style.display = 'none' ;
-                }
+            if(timer >= 491){
+                 parede.style.animation = 'parede-animation 0.8s infinite linear';
+                 olhos.style.display = 'none' ;
+            }
             }, 
             100);    
         } 
-    }  
-    
-    const pulo = ()=>{
-        itachi.classList.add('pulo');
-        setTimeout(() => {
+        
+            setInterval(() => {
+            const paredePosition = parede.offsetLeft;
+            const nuvensPosition = nuvens.offsetLeft;
+            const itachiPosition = +window.getComputedStyle(itachi).bottom.replace('px', '');
+            
+            
+            
+            if (paredePosition <= 80 && paredePosition > 0 && itachiPosition < 100) {
+                parede.style.animation = 'none';
+                parede.style.left = `${paredePosition}px`;
+                
+                itachi.style.animation = 'none';
+                itachi.style.bottom = `${itachiPosition}px`;
+                
+                nuvens.style.animation = 'none';
+                nuvens.style.left = `${nuvensPosition}px`;
+                
+                itachi.src = './img/itachi.gif';
+                itachi.style.width = '90px';
+                itachi.style.left = '15px';
+                
+                
+                pontuacao = timer 
+                localStorage.setItem('record', pontuacao);
+                
+                clearInterval(id);
+                olhos.style.display = 'block' ;
+                setTimeout(function(){
+                    gameover.style.display = 'block'}, 2000)
+                } 
+            }, 10);
+            
+            gameover.addEventListener('click', function(){
+                
+                iniciar()
+        })
+        }  )
+        
+        const pulo = ()=>{
+            itachi.classList.add('pulo');
+            setTimeout(() => {
             itachi.classList.remove('pulo');
         }, 500);
     };
+    let record = localStorage.getItem('record');
     
-    let pontuacao
-    const loop = setInterval(() => {
-        const paredePosition = parede.offsetLeft;
-        const nuvensPosition = nuvens.offsetLeft;
-        const itachiPosition = +window.getComputedStyle(itachi).bottom.replace('px', '');
-
-        
-        
-        if (paredePosition <= 80 && paredePosition > 0 && itachiPosition < 100) {
-            parede.style.animation = 'none';
-            parede.style.left = `${paredePosition}px`;
-            
-            itachi.style.animation = 'none';
-            itachi.style.bottom = `${itachiPosition}px`;
-            
-            nuvens.style.animation = 'none';
-            nuvens.style.left = `${nuvensPosition}px`;
-            
-            itachi.src = './img/itachi.gif';
-            itachi.style.width = '90px';
-            itachi.style.left = '15px';
-            
-            pontuacao = timer 
-            localStorage.setItem('record', pontuacao);
-            
-            clearInterval(id);
-            clearInterval(loop);
-            olhos.style.display = 'block' ;
-            setTimeout(function(){
-                gameover.style.display = 'block'}, 2000)
-            } 
-        }, 10);
-        
-        let record = localStorage.getItem('record');
-        
     document.addEventListener('keydown', pulo);
-    document.addEventListener('click', pulo);
-    gameover.addEventListener('click', function(){
-        window.location.reload(true)
-        maiorPontu()
-    })
+    document.addEventListener('click', pulo);          //Funcionalidade do game
+
+
+        // parede.style.animation = 'block';
+        
+        // itachi.style.animation = 'block';
+        
+        // nuvens.style.animation = 'block';
+        
+        // itachi.src = './img/itachi_running.gif';
+        
+        // clearInterval(loop);
+        
+        
+        // console.log(7)
+        // maiorPontuacao.textContent = record
     
-    function maiorPontu(){
-        maiorPontuacao.textContent = record
-    }
-   
+    
+    
+    
+    
     
